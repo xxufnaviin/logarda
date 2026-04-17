61a47f81ad15456863839e2e88522df47c95dd13
xxufnaviin <naviin2373@gmail.com>
2026-04-17 17:05:08 +0800

    update schema

--- database/schema.sql
+++ database/schema.sql
@@ -8,6 +8,16 @@ CREATE TABLE metrics (
     PRIMARY KEY (metricTime, instance_id) --composite primary key to prevent double entries at same timestamp for single instance
 );
 
+CREATE TABLE predicted_metrics (
+    metricTime TIMESTAMP NOT NULL,
+    instanceID VARCHAR NOT NULL,
+    cpu DOUBLE PRECISION NOT NULL,
+    network DOUBLE PRECISION NOT NULL,
+    memory DOUBLE PRECISION NOT NULL, 
+    
+    PRIMARY KEY (metricTime, instance_id) --composite primary key to prevent double entries at same timestamp for single instance
+);
+
 CREATE TABLE logs (
     eventTime TIMESTAMP NOT NULL,
     errorCode VARCHAR NOT NULL,-- only store error logs
