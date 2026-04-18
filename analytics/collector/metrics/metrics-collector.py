db53e1d701ac1d37c3f5f85fabe06133943fc228
xxufnaviin <naviin2373@gmail.com>
2026-04-18 16:39:03 +0800

    Update collector files

--- analytics/collector/metrics/metrics-collector.py
+++ analytics/collector/metrics/metrics-collector.py
@@ -1,17 +1,27 @@
-import psycopg2
+import postgres
+import boto3
+from datetime import datetime, timezone
+import pandas as pd
+
+AWS_REGION = "ap-southeast-1"
+      
+# establish connection with cloudwatch and ec2      
+cloudwatch = boto3.client("cloudwatch", region_name=AWS_REGION)
+ec2_client = boto3.client('ec2', region_name=AWS_REGION)
+
+# establish connection with PostgreSQL database
+# conn = 
 
 
-conn = psycopg2.connect(host="-.-.-.-",database="-",user="-",password="-")
-cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM metrics;")
+if __name__ == "__main__":
+    all_instances = []
+    ec2_response = ec2_client.describe_instances()
 
-data = cursor.fetchall()
-for i in data:
-    print(i)
+    for reservation in ec2_response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            host_name = f"ip-{instance['PrivateIpAddress'].replace('.','-')}"
+            all_instances.append((instance_id, host_name))
 
 
-conn.close()
-cursor.close()
-
-# insert into database here 
---
+++ analytics/collector/utils/postgres.py
@@ -0,0 +1,16 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
+
+def create_connection():
+    return psycopg2.connect(host=POSTGRES_HOST, 
+                        database=POSTGRES_DATABASE, 
+                        user=POSTGRES_USER, 
+                        password=POSTGRES_PASSWORD)
