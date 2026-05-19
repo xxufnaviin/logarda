acf37936e3d85736fd27fae9dea03c487488053b
xxufnaviin <naviin2373@gmail.com>
2026-05-19 23:49:09 +0800

    update scripts

--- scripts/deploy_ingestion.sh
+++ scripts/deploy_ingestion.sh
@@ -1,7 +1,8 @@
 # run from root
 # take env and place it in ec2
 
-scp -r -i "logarda.pem" config/.env $EC2_USER@$EC2_HOST:/home/$EC2_USER/config
+ssh -i logarda.pem $EC2_USER@$EC2_HOST "mkdir -p /home/$EC2_USER/config"
+scp -i logarda.pem config/.env $EC2_USER@$EC2_HOST:/home/$EC2_USER/config/.env
 
 # pull the latest docker image into the repo
 # add cron jobs to run containers and remove them after done
