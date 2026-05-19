8e4810f3beb4f51d0692a07ac2803a5ac5878870
xxufnaviin <naviin2373@gmail.com>
2026-05-19 23:55:40 +0800

    fix env path

--- scripts/deploy_ingestion.sh
+++ scripts/deploy_ingestion.sh
@@ -8,5 +8,5 @@ scp -i logarda.pem config/.env $EC2_USER@$EC2_HOST:/home/$EC2_USER/config/.env
 # add cron jobs to run containers and remove them after done
 ssh -i "logarda.pem" $EC2_USER@$EC2_HOST << EOF
 docker pull $ECR_IMAGE
-(echo "*/15 * * * * /usr/bin/docker run --env-file /home/ubuntu/ingestion/config/.env --rm $ECR_IMAGE main.py") | crontab -
+(echo "*/15 * * * * /usr/bin/docker run --env-file /home/ubuntu/config/.env --rm $ECR_IMAGE main.py") | crontab -
 EOF
