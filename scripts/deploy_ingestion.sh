a18555308d47888c842bb9929c6eb7e6a888f622
xxufnaviin <naviin2373@gmail.com>
2026-05-19 23:21:51 +0800

    deploy in ec2

--- scripts/deploy_ingestion.sh
+++ scripts/deploy_ingestion.sh
@@ -5,7 +5,7 @@ scp -r -i "logarda.pem" config/.env $EC2_USER@$EC2_HOST:/home/$EC2_USER/config
 
 # pull the latest docker image into the repo
 # add cron jobs to run containers and remove them after done
-ssh -i "logarda.pem" $EC2_USER@$EC2_HOST << 'EOF'
-docker pull repo:latest
-(echo "*/15 * * * * /usr/bin/docker run --env-file /home/ubuntu/ingestion/config/.env --rm collector:latest main.py") | crontab -
+ssh -i "logarda.pem" $EC2_USER@$EC2_HOST << EOF
+docker pull  ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ vars.AWS_REGION }}.amazonaws.com/${{ vars.ECR_REPOSITORY }}:latest
+(echo "*/15 * * * * /usr/bin/docker run --env-file /home/ubuntu/ingestion/config/.env --rm  ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ vars.AWS_REGION }}.amazonaws.com/${{ vars.ECR_REPOSITORY }}:latest main.py") | crontab -
 EOF
